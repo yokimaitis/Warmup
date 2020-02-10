2f1e7e712a9f136ea41f9019a227ad73498f444a
yokimaitis <Moren189la>
2020-02-10 17:22:30 +0300

    12

--- src/com/company/Calc.java
+++ src/com/company/Calc.java
@@ -7,16 +7,11 @@ public class Calc {
     public static void calculate() {
         List<String> inputMass = new ArrayList<>();
         inputMass = FileRead.read("C:\\Users\\kostya.GOMODS\\IdeaProjects\\Warmup\\src\\com\\company\\111.txt");
-        String numbers = "0123456789";
         String str = "";
-        String tmp = "";
         for (int i = 0; i < 1; i++) {
             str = inputMass.get(i);
-
             for (int j = 0; j < str.length(); j++) {
-                tmp = tmp + str.charAt(j);
-                if (numbers.contains(tmp)) WLN.write("Это число..");
-                tmp="";
+                if (Character.isDigit(str.charAt(j))) WLN.write("Это число..");
             }
 
         }
