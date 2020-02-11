f464d070f2caa01d559ad881f043eb3325a9d72f
yokimaitis <Moren189la>
2020-02-11 13:25:49 +0300

    12

--- src/com/company/Calc.java
+++ src/com/company/Calc.java
@@ -1,24 +1,112 @@
 package com.company;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Calc {
     List<String> inputMass = new ArrayList<>();
-    int oNumber=0;
+    int oNumber = 0;
+
+    float[] getTwo(String instr, int pos) {
+        float[] ar = new float[4];
+        instr = "<" + instr + ">";
+        for (int i = pos + 1; i < instr.length(); i++) {
+            if ((Character.isDigit(instr.charAt(i))) | instr.charAt(i) == '.') {
+            } else {
+                System.out.println("SecondNumber = " + instr.substring(pos + 1, i));
+                ar[1] = Float.valueOf(instr.substring(pos + 1, i));
+                ar[3] = pos;
+                System.out.println("ar[3] =" + pos);
+                break;
+            }
+        }
+        for (int j = pos - 1; j >= 0; j--) {
+            if ((Character.isDigit(instr.charAt(j))) | instr.charAt(j) == '.') {
+            } else {
+                System.out.println("FirstNumber = " + instr.substring(j + 1, pos));
+                ar[0] = Float.valueOf(instr.substring(j + 1, pos));
+                System.out.println("ar[2] =" + j);
+                ar[2] = j;
+                break;
+            }
+        }
+        return ar;
+    }
 
     void calculate() {
         inputMass = FileRead.read("111.txt");
         String str = "";
-        for (int i = 0; i < 1; i++) {
+        float x = 0.0f;
+        float[] out = new float[4];
+        for (int i = 0; i < inputMass.size(); i++) {
             str = inputMass.get(i);
+            oNumber = 0;
             for (int j = 0; j < str.length(); j++) {
-                if (Character.isDigit(str.charAt(j))) WLN.write("Это число..");
-                else oNumber++;
+                if (Character.isDigit(str.charAt(j))) {
+                } else oNumber++;
+            }
 
+            for (int xOper = 1; xOper <= oNumber; xOper++) {
+                if (str.indexOf('*') > 0) {
+                    System.out.println("Входная строка : " + str);
+                    System.out.println("pos = " + str.indexOf('*'));
+                    out = getTwo(str, str.indexOf('*') + 1);  //+1 Потому что в getTwo добавляем '<'
+                    x = out[0] * out[1];
+                    System.out.println("Перемножение = " + x);
+                    if (xOper == oNumber) str = String.valueOf(x);
+                    else {
+              //          System.out.println("Добавочное " + str.substring((int) out[3], str.length() - 1));
+                        str = str.substring(0, (int) out[2]) + x + str.substring((int) out[3], str.length() - 1);
+                    }
+                } else if (str.indexOf('/') > 0) {
+                    System.out.println("Входная строка : " + str);
+                    System.out.println("pos = " + str.indexOf('/'));
+                    out = getTwo(str, str.indexOf('/') + 1);  //+1 Потому что в getTwo добавляем '<'
+                    x = out[0] / out[1];
+                    System.out.println("Деление = " + x);
+                    if (xOper == oNumber) str = String.valueOf(x);
+                    else {
+               //         System.out.println("Добавочное " + str.substring((int) out[3], str.length() - 1));
+                        str = str.substring(0, (int) out[2]) + x + str.substring((int) out[3], str.length() - 1);
+                    }
+                } else if (str.indexOf('+') > 0) {
+                    System.out.println("Входная строка : " + str);
+                    System.out.println("pos = " + str.indexOf('+'));
+                    out = getTwo(str, str.indexOf('+') + 1);  //+1 Потому что в getTwo добавляем '<'
+                    x = out[0] + out[1];
+                    System.out.println("Суммируем = " + x);
+                    System.out.println("Добавочное " + str.substring((int) out[3], str.length() - 1));
+                    if (xOper == oNumber) str = String.valueOf(x);
+                    else {
+               //         System.out.println("Добавочное " + str.substring((int) out[3], str.length() - 1));
+                        str = str.substring(0, (int) out[2]) + x + str.substring((int) out[3], str.length() - 1);
+                    }
+
+                } else if (str.indexOf('-') > 0) {
+                    System.out.println("Входная строка : " + str);
+                    System.out.println("pos = " + str.indexOf('-'));
+                    out = getTwo(str, str.indexOf('-') + 1);  //+1 Потому что в getTwo добавляем '<'
+                    x = out[0] - out[1];
+                    System.out.println("Вычитаем = " + x);
+                    if (xOper == oNumber) str = String.valueOf(x);
+                    else {
+                //        System.out.println("Добавочное " + str.substring((int) out[3], str.length() - 1));
+                        str = str.substring(0, (int) out[2]) + x + str.substring((int) out[3], str.length() - 1);
+                    }
+                }
+                System.out.println("Результат = " + str);
             }
 
         }
+
+        //          if (str.charAt(j)=='*') {
+        ///              out=getTwo(str,j);
+        ///              x=out[0]*out[1];
+        ///             System.out.println("Перемножение = " + x);
+        //        }
+        //     out = getTwo("<245+34*66>",4);
     }
+
+
 }
+
